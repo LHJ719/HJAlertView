3257162fcffbf8e86a7d72277c67bb15b820287b
gedztr <lhj18910594271@163.com>
2019-01-08 14:00:32 +0800

    add pod files

---
+++ HJAlertView/HJAlertView.h
@@ -0,0 +1,17 @@
+//
+//  HJAlertView.h
+//  O2ViewDemo
+//
+//  Created by wandaios on 2019/1/8.
+//  Copyright © 2019年 HJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HJAlertView : UIView
++ (void)showAlert;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HJAlertView/HJAlertView.m
@@ -0,0 +1,28 @@
+//
+//  HJAlertView.m
+//  O2ViewDemo
+//
+//  Created by wandaios on 2019/1/8.
+//  Copyright © 2019年 HJ. All rights reserved.
+//
+
+#import "HJAlertView.h"
+
+@implementation HJAlertView
+
++ (void)showAlert {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"温馨提示" message:@"你是个好人" delegate:self cancelButtonTitle:@"" otherButtonTitles:@"确定", nil];
+    [alert show];
+}
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
